86dd12e2aceaebb52330a8141b229ce63abc9b99
Rajat <rajatbr13@gmail.com>
2026-06-29 11:31:09 +0530

    feat: add CORS to allow frontend connect with the backend

--- Backend/main.py
+++ Backend/main.py
@@ -1,5 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers.sessions import router as session_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(session_router)
