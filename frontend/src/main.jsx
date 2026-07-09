66b69ba873ce5957aef7811876a57b1b8823cda7
Rajat <rajatbr13@gmail.com>
2026-07-09 21:26:36 +0530

    fix(react): remove React strict mode for debugging

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,5 @@ import "./App.css";
 // It finds the <div id="root"> in index.html and renders
 // our <App /> component inside it.
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
